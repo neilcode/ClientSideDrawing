b9b29a7d24ebd45c414509d81fcf2692ae38cb09
Ahmed Abdelaal <ahmed1500@gmail.com>
2014-12-16 14:04:33 -0800

    files files fiiiiileessssss

---
+++ visualization/js/main.js
@@ -0,0 +1,32 @@
+// changing element font color
+
+// var paragraphs = document.getElementsByTagName("p");
+// for (var i = 0; i < paragraphs.length; i++) {
+//   var paragraph = paragraphs.item(i);
+//   paragraph.style.setProperty("color", "red", null);
+// }
+
+// d3.selectAll("p").style("color", "green");
+
+// d3.selectAll("p").style("visibility", "hidden");
+
+// var canvas = d3.select("body")
+//   .append("svg")
+//   .attr("width",500)
+//   .attr("height", 500)
+//   .attr("background-color", "red");
+// var circle = canvas
+//   .append("circle")
+//   .attr("cx", 50)
+//   .attr("cy",50)
+//   .attr("r",25);
+
+// circle.transition()
+//   .duration(2000)
+//   .attr("cx",300)
+//   .transition()
+//   .attr("cy",200)
+//   .attr("fill","red")
+//   .each("end", function(){
+//     alert("done!")
+//   })
